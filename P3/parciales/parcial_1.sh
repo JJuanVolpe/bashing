ba410d69ea10c122480634579df1e63457cd1ea4
Juan <juan.voolpe@gmail.com>
2022-10-13 08:55:32 -0300

    Practice for Test

---
+++ P3/parciales/parcial_1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+<< 'Comment'
+#Resuelvo enunciado en foto jpeg de ej1.jpeg
+#Basicamente verifico si hay parámetros y, si existen, veo que solo tengan mayúscula al comienzo, y sea un nombre correcto
+# 
+Comment
+
+for i in $*; do
+    echo "$i"                                                                                
+done
+echo "##################################################"
+if [[ "$#" != "0" ]]; then
+    counter=0
+    for i in $*; do
+        if [[ "${i}" =~ ^[A-Z] ]] && [[ ! "${i:(1):(${#i})}" =~ [A-Z] ]] && [[ ! "$i" =~ [0-9] ]]; then 
+            echo "$i"                                                                                
+            echo "OK" 
+        else
+            echo "$i no respeta el formato (Primera letra en mayúscula y el resto en minúsculas)"
+            counter=$(expr $counter + 1)
+        fi
+    done
+    echo "Muchas gracias. ud ingresó ${counter} dato(s) inválido(s)"
+fi
